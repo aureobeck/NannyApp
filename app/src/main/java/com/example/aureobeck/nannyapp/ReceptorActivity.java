dc920a9121a397c7da715205b5c2d34f60e255a6
Aureo Beck Figueiredo Souza <aureo@moblee.net>
2017-06-21 23:21:16 -0300

    Adds intensity processor and adds chronometer to get result in receptorActivity

--- app/src/main/java/com/example/aureobeck/nannyapp/ReceptorActivity.java
+++ app/src/main/java/com/example/aureobeck/nannyapp/ReceptorActivity.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import android.content.SharedPreferences;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -15,6 +16,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Chronometer;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
@@ -22,7 +24,6 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.aureobeck.nannyapp.Utils.CountDownTimerSkipFirst;
 import com.example.aureobeck.nannyapp.Utils.FirebaseClient;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -31,12 +32,13 @@ import com.firebase.client.ValueEventListener;
 
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.SilenceDetector;
 import be.tarsos.dsp.io.android.AudioDispatcherFactory;
 import be.tarsos.dsp.pitch.PitchDetectionHandler;
 import be.tarsos.dsp.pitch.PitchDetectionResult;
 import be.tarsos.dsp.pitch.PitchProcessor;
 
-public class ReceptorActivity extends AppCompatActivity implements CountDownTimerSkipFirst.OnFinishedInterface {
+public class ReceptorActivity extends AppCompatActivity {
 
     // ******   Views  *****
 
@@ -53,7 +55,7 @@ public class ReceptorActivity extends AppCompatActivity implements CountDownTime
     private static TextView textViewIntensityThreshold;
 
     private static TextView textViewFrequencyOutput;
-    private static TextView textViewIntervalOutput;
+    private static Chronometer chronometerIntervalOutput;
     private static TextView textViewIntensityOutput;
 
     private static ProgressBar progressBarResult;
@@ -62,22 +64,22 @@ public class ReceptorActivity extends AppCompatActivity implements CountDownTime
 
     Context ctx = this;
 
-    private final Long FREQUENCY_EVALUATION_INTERVAL = (long) 5000;
+    private final Long FREQUENCY_EVALUATION_INTERVAL = (long) 3000;
     private final Integer SAMPLE_RATE = 22050;
 
     private static Integer currentFrequencyThreshold = 1000;
-    private static Integer currentIntervalThreshold = 5;
+    private static Integer currentIntervalThreshold = 10;
     private static Integer currentIntensityThreshold = 0;
 
-    private static CountDownTimerSkipFirst countDownTimerFrequency;
-    private static CountDownTimer countDownTimerIntensity;
+    private static CountDownTimer countDownTimer;
+    private static long currentChronometerValue;
 
-    private static Double currentFrequencyIncrement = 1.0;
-    private static Double currentFrequencyAccumulated = 1.0;
-    private static Integer evaluationAccumulated = 0;
+    private static Double currentIncrement = 1.0;
 
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm:ss");
     private static Firebase firebaseRef;
+    private static AudioDispatcher dispatcher;
+    private static SilenceDetector silenceDetector;
 
     // ******   Inicialization Methods  *****
 
@@ -104,13 +106,18 @@ public class ReceptorActivity extends AppCompatActivity implements CountDownTime
         // *****   Inicialize Processing  *****
         configFrequencyEvaluatingCountDown();
         setIntervalThreshold(currentIntervalThreshold);
+        setIntensityThreshold(currentIntensityThreshold);
         setFrequencyThreshold(currentFrequencyThreshold / 95);
+        setDispatcher();
+        setIntensityProcessor();
         setFrequencyProcessor();
 
         // *****  Events  *****
         onFrequencyThresholdSeekBarChanged();
+        onIntensityThresholdSeekBarChanged();
         onIntervalThresholdSeekBarChanged();
         onConnectionSwitchPressed();
+        onChronometerTick();
 
     }
 
@@ -130,12 +137,12 @@ public class ReceptorActivity extends AppCompatActivity implements CountDownTime
         seekBarInterval = (SeekBar) findViewById(R.id.seekBarInterval);
 
         textViewFrequencyThreshold = (TextView) findViewById(R.id.textViewFrequencyConfiguration);
-        textViewIntervalThreshold = (TextView) findViewById(R.id.textViewIntervalConfiguration);
         textViewIntensityThreshold = (TextView) findViewById(R.id.textViewIntensityConfiguration);
+        textViewIntervalThreshold = (TextView) findViewById(R.id.textViewIntervalConfiguration);
 
         textViewFrequencyOutput = (TextView) findViewById(R.id.textViewFrequencyOutput);
-        textViewIntervalOutput = (TextView) findViewById(R.id.textViewIntervalOutput);
         textViewIntensityOutput = (TextView) findViewById(R.id.textViewIntensityOutput);
+        chronometerIntervalOutput = (Chronometer) findViewById(R.id.chronometerIntervalOutput);
 
         progressBarResult = (ProgressBar) findViewById(R.id.progressBarResult);
     }
@@ -156,13 +163,17 @@ public class ReceptorActivity extends AppCompatActivity implements CountDownTime
         seekBarInterval.setProgress(currentIntervalThreshold);
     }
 
-    private void setFrequencyProcessor() {
-        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(
+    // ******   DSP Methods  *****
+
+    private void setDispatcher() {
+        dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(
                 SAMPLE_RATE,
                 1024,
                 0
         );
+    }
 
+    private void setFrequencyProcessor() {
         dispatcher.addAudioProcessor(new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, SAMPLE_RATE, 1024, new PitchDetectionHandler() {
 
             @Override
@@ -172,16 +183,30 @@ public class ReceptorActivity extends AppCompatActivity implements CountDownTime
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        textViewFrequencyOutput.setText(pitchInHz + " Hz");
-                        if (pitchInHz > currentFrequencyThreshold) {
-                            if (evaluationAccumulated >= 5) {
-                                evaluationAccumulated = 0;
-                                setFrequencyEvaluatingCountDown();
-                            } else {
-                                evaluationAccumulated++;
-                            }
-                            textViewFrequencyOutput.setTextColor(getResources().getColor(R.color.red_01));
+                        textViewFrequencyOutput.setText(String.format("%.2f", pitchInHz) + " Hz");
+                        Double intensityInDb = silenceDetector.currentSPL();
+                        textViewIntensityOutput.setText(String.format("%.2f", intensityInDb)+" dB");
 
+                        if (intensityInDb > currentIntensityThreshold) {
+                            textViewIntensityOutput.setTextColor(getResources().getColor(R.color.red_01));
+                            if (pitchInHz > currentFrequencyThreshold) {
+                                // Is Crying - frequency and intensity greater than threshold)
+                                startChronometer();
+                                Log.i("LOG", "START_CHRONOMETER");
+                                resetEvaluatingCountDown();
+                                Log.i("LOG", "START_COUNT_DOWN");
+
+                            } else {
+                                // Not Crying at the moment
+                                stopChronometer();
+                                Log.i("LOG", "STOP_CHRONOMETER");
+                            }
+                        } else {
+                            textViewIntensityOutput.setTextColor(getResources().getColor(R.color.green_01));
+                        }
+
+                        if (pitchInHz > currentFrequencyThreshold) {
+                            textViewFrequencyOutput.setTextColor(getResources().getColor(R.color.red_01));
                         } else {
                             textViewFrequencyOutput.setTextColor(getResources().getColor(R.color.green_01));
                         }
@@ -192,13 +217,86 @@ public class ReceptorActivity extends AppCompatActivity implements CountDownTime
         new Thread(dispatcher, "Audio Dispatcher").start();
     }
 
+    private void setIntensityProcessor() {
+        silenceDetector = new SilenceDetector(currentIntensityThreshold, false);
+        dispatcher.addAudioProcessor(silenceDetector);
+    }
+
+    // ******  Chronometer  Methods  *****
+
+    private void startChronometer() {
+        chronometerIntervalOutput.setBase(SystemClock.elapsedRealtime()-currentChronometerValue);
+        chronometerIntervalOutput.start();
+    }
+
+    private void stopChronometer() {
+        currentChronometerValue = SystemClock.elapsedRealtime()- chronometerIntervalOutput.getBase();
+        chronometerIntervalOutput.stop();
+    }
+
+    private void resetChronometer() {
+        currentChronometerValue=0;
+        chronometerIntervalOutput.setBase(SystemClock.elapsedRealtime());
+        chronometerIntervalOutput.stop();
+    }
+
+    private void onChronometerTick() {
+        chronometerIntervalOutput.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                Double currentProgress = (double) chronometer.getBase()*currentIncrement;
+                progressBarResult.setProgress((int) SystemClock.elapsedRealtime() - currentProgress.intValue());
+                Log.i("LOG", "INCREMENT_CURRENT_BASE :" +chronometer.getBase());
+                Log.i("LOG", "INCREMENT_CURRENT_INCREMENT :" +currentIncrement);
+                Log.i("LOG", "INCREMENT_CURRENT_PROGRESS :" +currentProgress.intValue());
+            }
+        });
+    }
+
     // ******   Action Methods  *****
+
+    public void onConnectionSwitchPressed() {
+        switchConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!switchConnection.isChecked()) {
+                    relativeLayoutConnection.setBackgroundResource(R.drawable.custom_border_red);
+                    switchConnection.setText("Sem conexão");
+                } else {
+                    if (getSharedPreferencesFirebaseWriteId().equals("")) {
+                        setNewFirebaseId();
+                    } else {
+                        setFirebaseConnectionOn();
+                    }
+                }
+            }
+        });
+    }
 
     private void onFrequencyThresholdSeekBarChanged() {
         seekBarFrequency.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 setFrequencyThreshold(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void onIntensityThresholdSeekBarChanged() {
+        seekBarIntensity.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setIntensityThreshold(progress);
             }
 
             @Override
@@ -232,30 +330,11 @@ public class ReceptorActivity extends AppCompatActivity implements CountDownTime
         });
     }
 
-    @Override
-    public void onFinishedCount() {
-        currentFrequencyAccumulated = 0.0;
-        progressBarResult.setProgress(currentFrequencyAccumulated.intValue());
-        countDownTimerFrequency.cancel();
+    public void resetNotCrying() {
+        progressBarResult.setProgress(0);
+        resetChronometer();
+        countDownTimer.cancel();
         Log.i("LOG", "FINISH_COUNT");
-    }
-
-    public void onConnectionSwitchPressed() {
-        switchConnection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!switchConnection.isChecked()) {
-                    relativeLayoutConnection.setBackgroundResource(R.drawable.custom_border_red);
-                    switchConnection.setText("Sem conexão");
-                } else {
-                    if (getSharedPreferencesFirebaseWriteId().equals("")) {
-                        setNewFirebaseId();
-                    } else {
-                        setFirebaseConnectionOn();
-                    }
-                }
-            }
-        });
     }
 
     // ******   Firebase Methods  *****
@@ -314,25 +393,30 @@ public class ReceptorActivity extends AppCompatActivity implements CountDownTime
     }
 
     private void setIntervalThreshold(int progress) {
-
         currentIntervalThreshold = progress;
-//        Date date = new Date((currentFrequencyIncrement * 1000+""));
-//                textViewIntervalThreshold.setText(simpleDateFormat.format(date));
         textViewIntervalThreshold.setText(currentIntervalThreshold.toString());
-
-        currentFrequencyIncrement = ((double) 1 / (double) 100 * currentIntervalThreshold);
+        currentIncrement = (((double) 1 / ((double) 10 * currentIntervalThreshold)));
     }
 
-    private void setFrequencyEvaluatingCountDown() {
-        countDownTimerFrequency.cancel();
-        countDownTimerFrequency.mFirstTick = true;
-        countDownTimerFrequency.start();
-        Log.i("LOG", "START_COUNT");
+    private void resetEvaluatingCountDown() {
+        countDownTimer.cancel();
+        countDownTimer.start();
+    }
+
+    private void setIntensityThreshold (int progress) {
+        currentIntensityThreshold = progress-100;
+        textViewIntensityThreshold.setText(currentIntensityThreshold.toString());
     }
 
     private void configFrequencyEvaluatingCountDown() {
-        countDownTimerFrequency = new CountDownTimerSkipFirst(FREQUENCY_EVALUATION_INTERVAL, 250, true, currentFrequencyAccumulated, progressBarResult, currentFrequencyIncrement);
-        countDownTimerFrequency.onFinishedInterface = this;
+        countDownTimer = new CountDownTimer(FREQUENCY_EVALUATION_INTERVAL, 1000) {
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                resetNotCrying();
+            }
+        }.start();
     }
 
     // ******   General Methods  *****
